012ae661a40a690cacea2cd5a0f2bc7199ba39dc
Elliot <ejp4604@rit.edu>
2020-03-19 15:36:38 -0400

    Delete Objects
    Deletes objects as they pass the camera

---
+++ CGP2/Assets/Scripts/DeleteObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObject : MonoBehaviour
+{
+    public float zValue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Delete(zValue);
+    }
+
+    void Delete(float zValue)
+    {
+        if (transform.position.z < zValue)
+            Destroy(gameObject);
+    }
+}
