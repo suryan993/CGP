d726c604d30b21534a605ed4a098a86cef7c23bf
Elliot <ejp4604@rit.edu>
2020-03-19 21:26:52 -0400

    Gates
    Spawning
    - Separated object spawning to a different script
    - commented out redundant and unnecessary code
    Speed Display
    - Created Speed Info script
    - This script takes two button objects for bars and sets them in line with the velocity range determined by the code
    Gates
    - gates are handled in the player script
    - a range and time interval is set
    - after the time interval is hit, it checks if the slider is within the set range
    - if it is within range, it changes the range to be higher
    - if not within range, prints out "defeat"
    - if the player manages to stay within the range 3 times, prints out "victory"

---
+++ CGP2/Assets/Scripts/SpawnGratable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGratable : MonoBehaviour
+{
+    // prefabs for obstacles
+    public GameObject[] gratablePrefabs;
+    //transform of the planet object
+    Transform planet;
+
+    // values for determining position and movement of objects
+    float radius = 25;
+    float playerHeight = 0.25f;
+    float[] laneAngles = { 0.02f, 0.01f, 0.0f, -0.01f, -0.02f };
+    float gratableAngle = 7;
+
+    // the newest obstacle in the scene
+    GameObject lastGratable;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // sets planet transform and creates first obstacle
+        planet = transform;
+        lastGratable = CreateGratable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // chaecks the angle based on the last obstacle and checks if it is greater than
+        // the designated gratable angle.  If it is, spawn a new obstacle
+        if (Vector3.Angle(lastGratable.transform.up, Vector3.forward) > gratableAngle)
+        {
+            lastGratable = CreateGratable();
+        }
+    }
+
+    // creates a new obstacle
+    GameObject CreateGratable()
+    { 
+        // Create a new gratable object in front of the planet
+        GameObject gratable = Instantiate(gratablePrefabs[Random.Range(0, gratablePrefabs.Length)]);
+        gratable.transform.SetParent(planet);
+        gratable.transform.up = Vector3.forward;
+        gratable.transform.position = planet.position + Vector3.forward * (radius + playerHeight);
+        gratable.transform.RotateAround(planet.position, Vector3.up, 120 * laneAngles[Random.Range(0, laneAngles.Length)]);
+        return gratable;
+    }
+}
