2f8dc961c8d38fe40200b8b3f0c697963aa22322
rhetley <54686199+rhetley@users.noreply.github.com>
2020-03-21 17:03:35 -0400

    Reduced to one project folder

--- CGP/Assets/Scripts/Player.cs
+++ CGP/Assets/Scripts/Player.cs
@@ -1,101 +1,207 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Player : MonoBehaviour
 {
     public Transform planet; // drag the planet here
-    float radius =520; // planet radius
-    float vel = 6; // player speed - degrees per second
-    public GameObject[] prefabs; // drag the item prefabs here
-    int qntItems = 30; // how many items populate the scene
-    float bornAngle = 0; // items born at this X angle
-    float killAngle = 90; // items disappear after this angle
-    float pathAngle = 10; // path angle from vertical
-    float grassAngle = 45; // end of grass angle from vertical
-    public GameObject[] pickupPrefabs; // drag the pickup prefabs here
-    float pickupAngle = 7; // degrees between pickups
-    float pickupHeight = 1; // pickup height above ground
- 
+    Transform playerRotationCore; // allows the player to tilt around the surface of the planet
+    //float radius = 25; // planet radius
+    float currentVelocity; // player speed - degrees per second
+    float overallAcceleration;
+    float minVelocity = 5;
+    float maxVelocity = 50;
+    public SpeedInfo speedDisplay;
+    float[] laneAngles = { 0.02f, 0.01f, 0.0f, -0.01f, -0.02f }; // z values for where to rotate to be in each lane
+    int leftmostLane; // array index to show how far the player is allowed to move at present
+    int rightmostLane; // all these ints are array indices
+    int currentLane;
+    int destinationLane;
+    bool isChangingLeft = false;
+    bool isChangingRight = false;
+    float laneChangeVel = 6; // player speed in z rotating around the playerRotationCore
+    //public GameObject[] prefabs; // drag the item prefabs here
+    //int qntItems = 30; // how many items populate the scene
+    //float bornAngle = 0; // items born at this X angle
+    //float killAngle = 90; // items disappear after this angle
+    //float pathAngle = 10; // path angle from vertical
+    //float grassAngle = 45; // end of grass angle from vertical
+    public GameObject[] gratablePrefabs; // drag the gratable prefabs here
+    //float gratableAngle = 7; // degrees between gratable objects
+    //float playerHeight = 0.25f; // general height above ground where the player can be found
+    //GameObject lastGratable; // last gratable object created
+
+    [System.NonSerialized]
     public GameObject[] items;
-    GameObject lastPickup; // last pickup created
- 
+
+    // values used for determining range and time used by the gates
+    public float topRange;
+    public float botRange;
+
+    public int distanceToGate;
+    [System.NonSerialized]
+    public int localDistanceToGate;
+    [System.NonSerialized]
+    public float completedDistance;
+
     void Start()
     {
-        items = new GameObject[qntItems];
-        // populate planet
-        for (var i = 0; i < qntItems; i++)
-        {
-            // create a random item
-            GameObject item = Instantiate(prefabs[Random.Range(0, prefabs.Length)]);
-            MoveItem(item, Random.Range(bornAngle, killAngle)); // move it to a random position
-            item.transform.SetParent(planet); // child item to the planet
-            items[i] = item;
-        }
-        lastPickup = CreatePickup();
+        playerRotationCore = transform.parent;
+        leftmostLane = 0; // The number of lanes can change depending on conditions during play
+        rightmostLane = 4;
+        currentLane = 2;
+        overallAcceleration = 2; // Different conditions can have different default acceleration
+        currentVelocity = minVelocity;
+
+        // Set default display and time for the first gate
+        speedDisplay.MapToRange(botRange, topRange);
+        localDistanceToGate = distanceToGate;
+        speedDisplay.SetRange();
+        completedDistance = 0;
     }
 
     void Update()
     {
-        // rotate planet according to up/down arrow keys
-        float vAxis = Input.GetAxis("Vertical");
-        if (vAxis > 0.1)
-        { // if moving forward...
-            planet.transform.Rotate(-vAxis * vel * Time.deltaTime, 0, 0); // rotate planet
-            //animation.CrossFade("walk"); // play "walk" animation
-        }
-        else
+        // Rotate planet according to player's velocity and mark the amount of distance completed
+        planet.transform.Rotate(-currentVelocity * Time.deltaTime, 0, 0);
+        completedDistance += currentVelocity * Time.deltaTime;
+        //animation.CrossFade("walk"); // play "walk" animation
+        //animation.CrossFade("idle"); // else play "idle"
+
+        // Accelerate regularly
+        currentVelocity = currentVelocity + overallAcceleration * Time.deltaTime;
+        if (currentVelocity > maxVelocity)
         {
-            //animation.CrossFade("idle"); // else play "idle"
+            currentVelocity = maxVelocity;
         }
-        for (var i = 0; i < qntItems; i++)
+
+        // Checks and changes the gates
+        HandleGates();
+
+        // Check for and handle lane changes
+        float hAxis = Input.GetAxis("Horizontal");
+        if (!isChangingLeft && !isChangingRight)
         {
-            // if item passed the kill angle from Z axis...
-            if (Vector3.Angle(items[i].transform.up, Vector3.forward) > killAngle)
+            if (hAxis < -0.1 && currentLane > leftmostLane)
             {
-                // replant it at the born angle, at random position
-                MoveItem(items[i], bornAngle);
+                destinationLane = currentLane - 1;
+                isChangingLeft = true;
+            }
+            else if (hAxis > 0.1 && currentLane < rightmostLane)
+            {
+                destinationLane = currentLane + 1;
+                isChangingRight = true;
+            }
+
+        }
+        else if (isChangingLeft)
+        {
+            if (playerRotationCore.rotation.z >= laneAngles[destinationLane])
+            {
+                currentLane = destinationLane;
+                isChangingLeft = false;
+            }
+            else // Rotate player toward the new lane
+            {
+                playerRotationCore.Rotate(0, 0, laneChangeVel * Time.deltaTime);
             }
         }
-        if (Vector3.Angle(lastPickup.transform.up, Vector3.forward) > pickupAngle)
+        else if (isChangingRight)
         {
-            lastPickup = CreatePickup();
+            if (playerRotationCore.rotation.z <= laneAngles[destinationLane])
+            {
+                currentLane = destinationLane;
+                isChangingRight = false;
+            }
+            else // Rotate player toward the new lane
+            {
+                playerRotationCore.Rotate(0, 0, -laneChangeVel * Time.deltaTime);
+            }
+        }
+    }
+
+    // Increases the range for the gate
+    void ManipulateRange()
+    {
+        topRange += .2f;
+        botRange += .2f;
+    }
+
+    // Handles logic determining whether to change gates or end the game
+    void HandleGates()
+    {
+        speedDisplay.DisplayCurrentSpeed(currentVelocity, minVelocity, maxVelocity);
+
+        // multiplier of 8 is used to check if the localDistanceToGate has been multiplied more than 3 times
+        if (distanceToGate * 8 == localDistanceToGate)
+            Debug.Log("VICTORY"); // place a boolean here or call function to end game
+        // checks if current localDistanceToGate is equal to the current time 
+        else if (completedDistance >= localDistanceToGate)
+        {
+            // checks if the current speed of the player (ascertained from the slider value) 
+            // is within the top and bottom ranges.  If so, player has met the threshold for the next level
+            if (speedDisplay.slider.value > botRange && speedDisplay.slider.value < topRange)
+            {
+                // increase the range values for the next level
+                ManipulateRange();
+
+                // map the range from the slider values to the height of the slider
+                // this is used to properly position the bars along the slider to match the new range
+                speedDisplay.MapToRange(botRange, topRange);
+
+                // Changes the bars to match the new positions
+                speedDisplay.SetRange();
+
+                // Reset completed distance and double the previous localDistanceToGate value
+                completedDistance = 0;
+                localDistanceToGate *= 2;
+            }
+            else // if the current speed is NOT within range, player loses
+            {
+                Debug.Log("DEFEAT"); // bool or function to signify end of game
+                SceneManager.LoadScene("GameOver");
+            }
         }
     }
 
     // Move item to a random position in the grass, at elevation angleX
     void MoveItem(GameObject item, float angleX)
     {
-        float angleY;
-        if (Random.value < 0.5)
-        { // randomly select left or right sides
-            angleY = Random.Range(-grassAngle, -pathAngle);
-        }
-        else
-        {
-            angleY = Random.Range(pathAngle, grassAngle);
-        }
-        // calculate new item up direction
-        var dir = Quaternion.Euler(0, angleY, 0) * Vector3.forward;
-        dir = Quaternion.Euler(-angleX, 0, 0) * dir;
-        // set item position and rotation
-        item.transform.position = planet.transform.position + radius * dir;
-        item.transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
-        // set random size and rotation about Y 
-        item.transform.Rotate(0, Random.Range(0, 360), 0);
-        item.transform.localScale = Random.Range(0.4f, 1.5f) * Vector3.one;
-        // set a random color
-        Vector4 rndColor = Random.insideUnitSphere;
-        Renderer rndr = item.GetComponentInChildren<Renderer>();
-        rndr.material.color = rndColor;
+        //float angleY;
+        //if (Random.value < 0.5)
+        //{ // randomly select left or right sides
+        //    angleY = Random.Range(-grassAngle, -pathAngle);
+        //}
+        //else
+        //{
+        //    angleY = Random.Range(pathAngle, grassAngle);
+        //}
+        //// calculate new item up direction
+        //var dir = Quaternion.Euler(0, angleY, 0) * Vector3.forward;
+        //dir = Quaternion.Euler(-angleX, 0, 0) * dir;
+        //// set item position and rotation
+        //item.transform.position = planet.transform.position + radius * dir;
+        //item.transform.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        //// set random size and rotation about Y 
+        //item.transform.Rotate(0, Random.Range(0, 360), 0);
+        //item.transform.localScale = Random.Range(0.4f, 1.5f) * Vector3.one;
+        //// set a random color
+        //Vector4 rndColor = Random.insideUnitSphere;
+        //Renderer rndr = item.GetComponentInChildren<Renderer>();
+        //rndr.material.color = rndColor;
     }
 
-    GameObject CreatePickup()
-    { // create pickup in front of the planet
-        GameObject pickup = Instantiate(pickupPrefabs[Random.Range(0, pickupPrefabs.Length)]);
-        pickup.transform.SetParent(planet);
-        pickup.transform.up = Vector3.forward;
-        pickup.transform.position = planet.position + Vector3.forward * (radius + pickupHeight);
-        return pickup;
+    void OnTriggerEnter(Collider objectHit)
+    {
+        GratableObject objectQualities = objectHit.GetComponent<GratableObject>();
+        if (objectQualities != null)
+        {
+            currentVelocity = currentVelocity - objectQualities.slowdownOnHit;
+            if (currentVelocity < minVelocity)
+            {
+                currentVelocity = minVelocity;
+            }
+        }
     }
 }
