c1fa6fe173488b1f86e02c2289464b1c0192623c
Elliot <ejp4604@rit.edu>
2020-04-14 10:36:58 -0400

    Added Credits
    Credited Music
    Created Script that opens URLs to sources

---
+++ CGP/Assets/Scripts/OpenURL.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public string URL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenTheURL()
+    {
+        Application.OpenURL(URL);
+    }
+}
